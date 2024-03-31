638170d0ed0f74939a04d465d980847a39821b57
ESPRZ <siyuan.zhao.syesprz@gmail.com>
2024-03-31 18:07:24 -0400

    Change Skillbars into SkillsTagClouds

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,23 +1,28 @@
+"use client"
 import React from 'react'
 import SkillList from './SkillList'
+import ToolsTagCloud from './TagCloud'
 
 const AboutSection = () => {
   return (
     <section id="about" className='text-black'>
-        <div className='container py-8 px-4 xl:gap-16 sm:py-16 lg:px-16 xl:px-60'>
-            <h2 className='section-title'>About Me</h2>
+      <div className='container py-8 px-4 xl:gap-16 sm:py-16 lg:px-16 xl:px-60'>
+        <h2 className='section-title'>About Me</h2>
 
-            <div className='grid md:grid-cols-2 justify-center mx-auto'>
-              <div>
-                <ul className='text-base lg:text-lg space-y-5 mt-4 mb-8 md:ml=[15%] lg:ml-[20%]'>                
-                  <li>&#8226;  Math @ Uwaterloo GPA: 4.0/4.0</li>
-                  <li>&#8226;  Winner @ Hackher2024</li>
-                  <li>&#8226;  Photographer @ CSC</li>
-                </ul>
-              </div>
-              <SkillList className='flex justify-end'/>
-            </div>
+        <div className='grid md:grid-cols-12 justify-center mx-auto'>
+          <div className='col-span-4'>
+            <ul className='text-base lg:text-lg space-y-5 mt-4 mb-8 md:ml=[5%] lg:ml-[10%]'>
+              <li>&#8226;  Math @ Uwaterloo GPA: 4.0/4.0</li>
+              <li>&#8226;  Winner @ Hackher2024</li>
+              <li>&#8226;  Photographer @ CSC</li>
+            </ul>
+          </div>
+          {/*<SkillList className='flex justify-end'/>*/}
+          <div className='col-span-8'>
+            <ToolsTagCloud />
+          </div>
         </div>
+      </div>
     </section>
   )
 }
---
+++ src/app/components/TagCloud.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from 'react'
+import TagCloud, { renderData } from 'd3-tagcloud-for-react';
+import VisibilitySensor from 'react-visibility-sensor';
+
+const colorarray = [
+    '#a3a3a3',
+    '#737373',
+    '#525252',
+    '#404040',
+    '#262626',
+    '#171717',
+    '#0a0a0a',
+]
+const data = [
+    {
+        label: 'C/C++',
+        fontSize: 9,
+        opacity: 10,
+    },
+    {
+        label: 'Dart',
+        fontSize: 4,
+        opacity: 5,
+    },
+    {
+        label: 'Flutter',
+        fontSize: 7,
+        opacity: 7,
+    },
+    {
+        label: 'HTML',
+        fontSize: 4,
+        opacity: 4,
+    },
+    {
+        label: 'CSS',
+        fontSize: 4,
+        opacity: 5,
+    },
+    {
+        label: 'React',
+        fontSize: 7,
+        opacity: 7,
+    },
+    {
+        label: 'JavaScript',
+        fontSize: 4,
+        opacity: 8,
+    },
+    {
+        label: 'Shell',
+        fontSize: 5,
+        opacity: 6,
+    },
+    {
+        label: 'Python',
+        fontSize: 8,
+        opacity: 9,
+    },
+    {
+        label: 'Tensorflow',
+        fontSize: 6,
+        opacity: 3,
+    },
+    {
+        label: 'Git',
+        fontSize: 6,
+        opacity: 4,
+    },
+    {
+        label: 'Numpy',
+        fontSize: 3,
+        opacity: 5,
+    },
+    {
+        label: 'Flask',
+        fontSize: 4,
+        opacity: 5,
+    },
+    {
+        label: 'SQL',
+        fontSize: 6,
+        opacity: 6,
+    },
+    {
+        label: 'Next.js',
+        fontSize: 3,
+        opacity: 4,
+    },
+    {
+        label: 'GraphQL',
+        fontSize: 3,
+        opacity: 4,
+    },
+]
+
+const ToolsTagCloud = () => {
+
+    const [visible, setVisible] = useState(false);
+    const onVisibilitySensorChange = (isVisible) => {
+        if (isVisible) {
+            setVisible(true);
+        }
+    };
+    return (
+        <div>
+            <VisibilitySensor
+                partialVisibility
+                offset={{ bottom: 80 }}
+                onChange={onVisibilitySensorChange}>
+                <TagCloud
+                    className='tag-cloud'
+                    style={{
+                        fontFamily: 'sans-serif',
+                        padding: 5,
+                        width: '100%',
+                        height: '300px',
+                    }}
+                    data={data}
+                    colorarray={colorarray}
+                >
+                    {renderData(data)}
+                </TagCloud>
+            </VisibilitySensor>
+
+
+        </div>
+    )
+}
+
+export default ToolsTagCloud
